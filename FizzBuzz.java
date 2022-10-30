2bbfb5afefb00967fbcb658b93c9574053a0c499
EC2 Default User <shinya.sin09@gmail.com>
2022-10-30 09:24:52 +0000

    [finish]input

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,39 +1,48 @@
-// import java.util.Scanner;
-// import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.util.InputMismatchException;
 
 class FizzBuzz{
   public static void main(String[] args){
-    int end = 30;
-    for(int x = 1; x <= end; x++){
-      if(x % 3 == 0 && x % 5 == 0){
+    end(10);
+
+    input();
+  }
+
+
+  public static void end(int end){
+    for(int i = 1; i <= end; i++){
+      if(i % 3 == 0 && i % 5 == 0){
         System.out.println("FizzBuzz");
-      }else if(x % 3 == 0){
+      }else if(i % 3 == 0){
         System.out.println("Fizz");
-      }else if(x % 5 == 0){
+      }else if(i % 5 == 0){
         System.out.println("Buzz");
       }else{
-        System.out.println(x);
+        System.out.println(i);
       }
     }
-
-    // try {
-    //   Scanner scanner = new Scanner(System.in);
-    //   System.out.print("Input > ");
-
-    //   int x = scanner.nextInt();
-
-    //   if(x % 15 == 0){
-    //     System.out.println("FizzBuzz");
-    //   }else if(x % 3 == 0){
-    //     System.out.println("Fizz");
-    //   }else if(x % 5 == 0){
-    //     System.out.println("Buzz");
-    //   }else{
-    //     System.out.println(x);
-    //   }
-    //   scanner.close();
-    //   }catch(InputMismatchException e){
-    //     System.out.println("整数を入力してください。");
-    //   }
-    }
   }
+
+
+    public static void input(){
+      try{
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input > ");
+
+        int i = scanner.nextInt();
+
+        if(i % 15 == 0){
+          System.out.println("FizzBuzz");
+        }else if(i % 3 == 0){
+          System.out.println("Fizz");
+        }else if(i % 5 == 0){
+          System.out.println("Buzz");
+        }else{
+          System.out.println(i);
+        }
+        scanner.close();
+        }catch(InputMismatchException e){
+        System.out.println("整数を入力してください。");
+        }
+    }
+}
